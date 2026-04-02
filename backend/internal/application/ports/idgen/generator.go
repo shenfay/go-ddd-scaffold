968614a38717d26b7a4ee33350fbb0f67cb5b743
shenfay <shengfai@qq.com>
2026-04-02 09:25:27 +0800

    refactor: 清空业务代码准备重构
    - 删除 domain 层所有领域模型代码
    - 删除 application 层所有应用服务代码
    - 删除 interfaces 层所有 HTTP 接口代码
    - 保留 infrastructure 基础设施层
    - 保留配置、工具类、数据库迁移等框架代码
    
    为后续 DDD 重构做准备

--- backend/internal/application/ports/idgen/generator.go
+++
@@ -1,7 +0,0 @@
-package idgen
-
-// Generator ID 生成器端口
-type Generator interface {
-	// Generate 生成唯一 ID
-	Generate() (int64, error)
-}
--- backend/internal/domain/common/value_object.go
+++
@@ -1,7 +0,0 @@
-package common
-
-// ValueObject 值对象标记接口
-// 值对象应该是不可变的，通过属性判断相等性
-// 注意：这是一个标记接口，具体的 Equals 方法由各值对象自行实现
-type ValueObject interface {
-}
